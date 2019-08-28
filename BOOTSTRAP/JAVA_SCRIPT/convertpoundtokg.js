4375ee483861cd8eb7c1180eeb720782bf39261e
shivaprasadkyathanatty <shivaprasad@acelearningco.com>
2019-08-28 12:01:34 +0530

    initial commit

---
+++ BOOTSTRAP/JAVA_SCRIPT/convertpoundtokg.js
@@ -0,0 +1,5 @@
+var pound=prompt("Enter a number in Pound")
+var kg=pound*0.454
+alert("You entered number in pound is "+pound+" it's converted value in KG is "+kg)
+console.log("Conversion Completed")
+console.log("Checking Github")
---
+++ JAVA_SCRIPT/first.js
@@ -0,0 +1,3 @@
+alert("Welcome to our Bank!!");
+var deposit=prompt("Enter Amount you want to deposit");
+console.log("Customer Deposited "+deposit)
