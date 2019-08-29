bcd2ee2be7af454618d0358583d6daaf0a961358
shivaprasadkyathanatty <shivaprasad@acelearningco.com>
2019-08-29 15:21:22 +0530

    project

---
+++ JAVA_SCRIPT/project_spy.js
@@ -0,0 +1,23 @@
+var fn=prompt('Enter your First Name');
+var ln=prompt('Enter your Last Name');
+var ag=prompt('Enter your age');
+var ht=prompt('Enter your height');
+var pt=prompt('Enter your pet name');
+
+console.log(fn[0])
+console.log(ln[0])
+console.log(ag)
+console.log(ht)
+console.log(pt.charAt(pt.length-1)
+
+
+
+
+if (fn[0]==ln[0] && ag>20 && ag<30 && ht>170 &&
+      pt.charAt(pt.length-1)=='y')
+      {
+        console.log('Welcome Comrade!!')
+      }
+else {
+  console.log('Sorry')
+}
