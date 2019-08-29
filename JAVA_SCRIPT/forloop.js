b2e4be7527aa86124033865c980f84c696b686c3
shivaprasadkyathanatty <shivaprasad@acelearningco.com>
2019-08-29 12:20:37 +0530

    for loop

---
+++ JAVA_SCRIPT/forloop.js
@@ -0,0 +1,13 @@
+for (var x=0; x<5; x++ ){
+  console.log(x)
+}
+
+var word="ABABABABABABA"
+
+for (var i = 0; i < word.length; i++) {
+    console.log(word[i]);
+}
+
+for (var i = 0; i < word.length; i=i+2) {
+  console.log(word[i]);
+}
--- JAVA_SCRIPT/second.js
+++ JAVA_SCRIPT/second.js
@@ -1,6 +1,13 @@
 var x=0
 while (x<5){
 console.log("the current number is: "+x)
+
+if (x==3){
+  console.log("x reached THREE");
+  break;
+}
+
 console.log("adding 1 to current number")
 x=x+1
+
 }
