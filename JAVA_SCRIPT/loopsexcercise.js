cbd4c6d3456929800cc3ea2cae35b9a83a3b0558
shivaprasadkyathanatty <shivaprasad@acelearningco.com>
2019-08-29 14:15:22 +0530

    loops excercise

---
+++ JAVA_SCRIPT/loopsexcercise.js
@@ -0,0 +1,17 @@
+i=0;
+while (i<5){
+  console.log("hello");
+  i=i+1;
+}
+
+for (var i=0;i<5;i++){
+  console.log("hello");
+}
+
+i=1;
+while(i<26){
+  if (i%2!=0){
+    console.log(i)
+  }
+i=i+1;
+}
