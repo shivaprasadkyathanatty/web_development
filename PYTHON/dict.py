90f25e9714f312ada4ac17cdd781b849e1211a3c
shivaprasadkyathanatty <shivaprasad@acelearningco.com>
2019-09-05 12:02:15 +0530

    dict

---
+++ PYTHON/dict.py
@@ -0,0 +1,13 @@
+mydict={"key1":"value1","key2":"value2"}
+print (mydict)
+print(mydict["key1"])
+secdict={"key1":"value1","key2":{"key3":[1,2,3]}}
+print(secdict)
+print(secdict["key2"])
+print(secdict["key2"]["key3"][0])
+food_dict={"lunch":"pizza","bfast":"vaggarane"}
+print(food_dict["lunch"])
+food_dict["lunch"]="vade payasa"
+print(food_dict["lunch"])
+food_dict["dinner"]="nuggekai saaru"
+print(food_dict)
