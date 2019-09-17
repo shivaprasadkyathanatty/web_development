00c2e8350e22d35704ea372dd4df919c6a1d030a
shivaprasadkyathanatty <shivaprasad@acelearningco.com>
2019-09-17 12:43:22 +0530

    excercise

--- DJANGO/Django_level_two/ProTwo/appTwo/views.py
+++ DJANGO/Django_level_two/ProTwo/appTwo/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from appTwo.models import User
 # Create your views here.
+def index(request):
+    return render(request,'appTwo/index.html')
+
+def users(request):
+    user_list=User.objects.order_by('first_name')
+    user_dict = {'users':user_list}
+    return rendr(request,'appTwo/users.html',context=user_dict)
