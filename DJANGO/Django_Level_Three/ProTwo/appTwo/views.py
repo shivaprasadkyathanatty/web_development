986e8c9dd15bdae9ffd1f616d6faba94b54370aa
shivaprasadkyathanatty <shivaprasad@acelearningco.com>
2019-09-20 16:36:17 +0530

    django final

---
+++ DJANGO/Django_Level_Three/ProTwo/appTwo/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+# from appTwo.models import User
+# Create your views here.
+from appTwo.forms import NewUserForm
+def index(request):
+    return render(request,'appTwo/index.html')
+
+def users(request):
+    form = NewUserForm()
+    if request.method == 'POST':
+        form = NewUserForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print('ERROR FROM INVALID')
+    return render(request,'appTwo/users.html',{'form':form})
