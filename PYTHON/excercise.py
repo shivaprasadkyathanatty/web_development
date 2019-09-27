3fe439f0b050c0afaa29964fed1ee4e583ff8157
shivaprasadkyathanatty <shivaprasad@acelearningco.com>
2019-09-27 11:29:54 +0530

    blockhain

--- PYTHON/excercise.py
+++ PYTHON/excercise.py
@@ -10,8 +10,7 @@ print(s[::2])
 l=[3,7,[1,4,'hello']]
 l[2][2]="goodbye"
 print(l)
-
-
+print("hello")
 d1={"simple_key":"hello"}
 d2={"k1":{"k2":"hello"}}
 d3={"k1":[{"nest_key":["this is deep",["hello"]]}]}
--- PYTHON/numbers.py
+++ PYTHON/numbers.py
@@ -1,4 +1,4 @@
 print(2+1)
 a = 5
 a=a*5
-print (a)
+print(a)
